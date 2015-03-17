32feaa27e6cf3a0dc6c6a5cb8c4202b2e500c681
Xun Chen <xun.revive@gmail.com>
2015-03-17 16:54:41 +0800

    add scripts to add/remove detectors in the source tree.

---
+++ scripts/disable_detector.sh
@@ -0,0 +1,44 @@
+#!/bin/sh
+
+if [ -z $BAMBOO_SOURCE ]
+then
+    BAMBOO_SOURCE=$PWD
+fi
+
+if [ -z $1 ]
+then
+    echo "please provide the detector set"
+    exit
+else
+    det_set=$1
+fi
+
+if [ ! -d $BAMBOO_SOURCE/include/detector ]
+then
+   echo "Please make sure you are in the top level of the BambooMC directory or have set the variable of BAMBOO_SOURCE"
+   exit
+fi
+   
+cd $BAMBOO_SOURCE/include/detector
+
+for a in $BAMBOO_SOURCE/detectors/$det_set/*.hh
+do
+    bname=$(basename $a)
+    if [ -e $bname ]
+    then
+        echo "remove symbolic link for $bname..."
+        rm $bname
+    fi
+done
+
+cd $BAMBOO_SOURCE/src/detector
+
+for a in $BAMBOO_SOURCE/detectors/$det_set/*.cc
+do
+    bname=$(basename $a)
+    if [ -e $bname ]
+    then
+        echo "remove symbolic link for $bname..."
+        rm $bname
+    fi
+done
