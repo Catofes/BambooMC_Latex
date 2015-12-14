7c71fab992775766ac1de470e03fa296197b4e69
Xun Chen <chenxun@sjtu.edu.cn>
2015-12-14 17:07:39 +0800

    fixed a bug causing memory leak
    1. The bug is in PandaXDataManager.
    2. Only appear when breaking long decay chain, the hits are not really
    cleared after partial event has been saved.

--- src/analysis/PandaXDataManager.cc
+++ src/analysis/PandaXDataManager.cc
@@ -356,9 +356,17 @@ void PandaXDataManager::resetPartialEvent(const G4Event * aEvent)
     G4VHitsCollection * hitsCollection = hCthis->GetHC(i);
     if (hitsCollection->GetName().contains("EnergyDepositionHits")) {
       PandaXEnergyDepositionHitsCollection * hC = (PandaXEnergyDepositionHitsCollection*)hitsCollection;
+      for (int i=0; i<hC->entries(); ++i) {
+        PandaXEnergyDepositionHit * hit = (*hC)[i];
+        delete hit;
+      }
       hC->GetVector()->clear();
     } else if (hitsCollection->GetName().contains("FlatSurfaceFluxHits")) {
       PandaXFlatSurfaceFluxHitsCollection * hC = (PandaXFlatSurfaceFluxHitsCollection*)hitsCollection;
+      for (int i=0; i<hC->entries(); ++i) {
+        PandaXFlatSurfaceFluxHit * hit = (*hC)[i];
+        delete hit;
+      }
       hC->GetVector()->clear();
     }    
   }
